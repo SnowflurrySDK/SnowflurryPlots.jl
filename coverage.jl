507d34cd14171d827d5573bc4ee565d0e54aa4b8
Nicolas Poirier <nicolas@anyonsys.com>
2023-05-01 16:53:32 -0400

    fix(coverage): correct file paths
    Issue #1

--- coverage.jl
+++ coverage.jl
@@ -4,19 +4,19 @@ using SnowflakePlots
 
 Pkg.test("SnowflakePlots"; coverage = true)
 # process '*.cov' files
-coverage = process_folder("SnowflakePlots.jl/src") # defaults to src/; alternatively, supply the folder name as argument
+coverage = process_folder("src") # defaults to src/; alternatively, supply the folder name as argument
 # coverage = append!(coverage, process_folder("deps"))  # useful if you want to analyze more than just src/
 # process '*.info' files, if you collected them
 coverage = merge_coverage_counts(
     coverage,
     filter!(
-        let prefixes = (joinpath(pwd(), "SnowflakePlots.jl/src", ""))
+        let prefixes = (joinpath(pwd(), "src", ""))
             c -> any(p -> startswith(c.filename, p), prefixes)
         end,
-        LCOV.readfolder("SnowflakePlots.jl/test"),
+        LCOV.readfolder("test"),
     ),
 )
-LCOV.writefile("SnowflakePlots.jl/lcov.info", coverage)
+LCOV.writefile("lcov.info", coverage)
 
 # Get total coverage for all Julia files
 covered_lines, total_lines = get_summary(coverage)
@@ -24,10 +24,10 @@ covered_lines, total_lines = get_summary(coverage)
 # @show get_summary(process_file(joinpath("src", "Snowflake.jl")
 
 #clean .cov files
-clean_folder("SnowflakePlots.jl/src")
-clean_folder("SnowflakePlots.jl/test")
 clean_folder("src")
 clean_folder("test")
-clean_folder("protobuf")
 
-return covered_lines, total_lines
+# Print summary
+println("Covered lines: $(covered_lines)")
+println("Total lines: $(total_lines)")
+println("Coverage percentage: $(covered_lines/total_lines)")
